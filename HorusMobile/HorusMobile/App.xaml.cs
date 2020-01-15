af1c871d59d20480f2ca0ddc3abc1b3102dc6131
Sulfur0 <aevega1991@gmail.com>
2020-01-15 11:12:10 -0300

    Commit inicial, Notificaciones push implementadas usando firebase y appcenter

---
+++ HorusMobile/HorusMobile/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using HorusMobile.Services;
+using HorusMobile.Views;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Push;
+namespace HorusMobile
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            AppCenter.Start("66dd0120-23ef-484f-a21c-e9d4f19a2e36", typeof(Push));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
