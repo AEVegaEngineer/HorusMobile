bef8baf6442d2c81a9bb547700bcd1f5731c04d2
Sulfur0 <aevega1991@gmail.com>
2020-01-23 13:08:49 -0300

    Login autenticando

--- HorusMobile/HorusMobile/Models/Users.cs
+++ HorusMobile/HorusMobile/Models/Users.cs
@@ -6,9 +6,11 @@ namespace HorusMobile.Models
 {
     public class Users
     {
+        public string id { get; set; }
         public string username { get; set; }
-        public string nombre { get; set; }
-        public string apellido { get; set; }
-        public DateTime fecha_nacimiento { get; set; }
+        public string password { get; set; }        
+        public string estado { get; set; }
+        public string tipo { get; set; }
+        public string fk_personal { get; set; }
     }
 }
--- HorusMobile/HorusMobile/Views/LoginPage.xaml.cs
+++ HorusMobile/HorusMobile/Views/LoginPage.xaml.cs
@@ -12,7 +12,9 @@ using System.ComponentModel;
 using HorusMobile.Models;
 using HorusMobile.Views;
 using HorusMobile.ViewModels;
-
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
 
 namespace HorusMobile.Views
 {
@@ -24,76 +26,92 @@ namespace HorusMobile.Views
             InitializeComponent();
         }
 
-        /*
-        protected override void OnAppearing()
-        {
-            base.OnAppearing();
-
-            Device.BeginInvokeOnMainThread(async () =>
-            {
-                RestClient client = new RestClient();
-                var getUserLogin = await client.Get<getUserLogin>("http://192.168.50.98/intermedio/api/usuarios/login.php");
-                if (getUserLogin != null)
-                {
-                    LabelChange.Text = getUserLogin.data.id;
-                    Debug.WriteLine("\n\nejecutado correctamente: " + getUserLogin.data.id);
-                }
-                else
-                {
-                    Debug.WriteLine("\n\nMAINPAGE NULL ERROR\n\n");
-                }
-            });
-
-            
-        }
-        */
-
         async void OnLoginButtonClicked(object sender, EventArgs e)
         {
             
             var user = username.Text;
             var pass = password.Text;
-            Device.BeginInvokeOnMainThread(async () =>
-            {
-                RestClient client = new RestClient();
-                var getUserLogin = await client.Get<getUserLogin>("http://192.168.50.98/intermedio/api/usuarios/login.php");
-                if (getUserLogin != null)
-                {
-                    LabelChange.Text = getUserLogin.data.id;
-                    Debug.WriteLine("\n\nlogin ejecutado correctamente: "+getUserLogin.data.id);
-                }
-                else
-                {
-                    Debug.WriteLine("\n\nMAINPAGE NULL ERROR\n\n");
-                }
-            });
-            /*
-            if (!string.IsNullOrWhiteSpace(user) && !string.IsNullOrWhiteSpace(pass))
-            {              
-                if (user == "10" & pass == "10")
-                {
-                    Debug.WriteLine("logeado");
-                    try
-                    {
-                        await Navigation.PushModalAsync(new MainPage());
-
-                        await Navigation.PopAsync();
-                    }
-                    catch (Exception ex)
-                    {
-                        Debug.WriteLine(ex);
-                    }
-                } else {
-                    await DisplayAlert("Login", "Usuario o pass incorrecto", "OK");
-                    Debug.WriteLine("Usuario o pass incorrecto"); 
-                }
-            }
-            else
+            
+            if (string.IsNullOrWhiteSpace(user) && string.IsNullOrWhiteSpace(pass))
             {
                 await DisplayAlert("Login", "Debe escribir un usuario y una contraseña", "OK");
                 Debug.WriteLine("Debe escribir un usuario y una contraseña");
+                return;
+            }
+            /*
+            else
+            {
+                if (user == "10" & pass == "10")
+                {
+                    
+                }
+                else
+                {
+                    await DisplayAlert("Login", "Usuario o pass incorrecto", "OK");
+                    Debug.WriteLine("Usuario o pass incorrecto");
+                }
             }     
             */
+            
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                //RestClient client = new RestClient();
+                HttpClient client = new HttpClient();
+
+                //var getUserLogin = await client.Get<getUserLogin>("http://192.168.50.98/intermedio/api/usuarios/login.php");
+                Users usuario = new Users();
+                usuario.password = pass;
+                usuario.username = user;
+                loading.Color = Color.Orange;
+                loading.IsRunning = true;
+
+                //serializo el objeto a json
+                var myContent = JsonConvert.SerializeObject(usuario);
+
+                //construyo un objeto contenido para mandar la data, uso un objeto ByteArrayContent
+                var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+                var byteContent = new ByteArrayContent(buffer);
+
+                //establezco el tipo de contenido a JSON para que la api la reconozca
+                byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                //envío el request por POST
+                var result = client.PostAsync("http://192.168.50.98/intermedio/api/usuarios/login.php", byteContent).Result;
+
+                if (result != null)
+                {
+                    loading.IsRunning = false;
+                    var contents = await result.Content.ReadAsStringAsync();                    
+                    Debug.WriteLine("\n\nlogin ejecutando:"+ contents);
+
+                    //Deserializo el JSON resultante para obtener los datos del usuario
+                    Users u = JsonConvert.DeserializeObject<Users>(contents);
+                    if (u.id == null)
+                    {
+                        await DisplayAlert("Login", "Usuario o pass incorrecto", "OK");
+                        Debug.WriteLine("Usuario o pass incorrecto");
+                    }
+                    else
+                    {
+                        try
+                        {                            
+                            await Navigation.PushModalAsync(new MainPage());
+
+                            await Navigation.PopAsync();
+                        }
+                        catch (Exception ex)
+                        {
+                            Debug.WriteLine(ex);
+                        }
+                    }
+
+                }
+                else
+                {
+                    Debug.WriteLine("\n\nRESULT NULL ERROR\n\n");
+                }
+            });
+            
 
         }
 
