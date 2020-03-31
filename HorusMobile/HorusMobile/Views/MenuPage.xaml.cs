588327038dc232bf23deb1f0348187b927effbb3
Sulfur0 <aevega1991@gmail.com>
2020-03-31 08:20:56 -0300

    creación de versiones para publicación

--- HorusMobile/HorusMobile/Views/MenuPage.xaml.cs
+++ HorusMobile/HorusMobile/Views/MenuPage.xaml.cs
@@ -9,7 +9,7 @@ namespace HorusMobile.Views
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    [Preserve(AllMembers = true)]
+    //[Preserve(AllMembers = true)]
     public partial class MenuPage : ContentPage
     {
         MainPage RootPage { get => Application.Current.MainPage as MainPage; }
