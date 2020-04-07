16f0f26e17a892e32324a1dfd9d853e8f95b127b
Sulfur0 <aevega1991@gmail.com>
2020-04-07 11:02:43 -0300

    agregado logo full formateado

--- HorusMobile/HorusMobile.Android/MainActivity.cs
+++ HorusMobile/HorusMobile.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using Com.OneSignal;
 
 namespace HorusMobile.Droid
 {
-    [Activity(Label = "HorusMobile", Icon = "@drawable/xamarin_logo", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "HorusMobile", Icon = "@drawable/cmsj_formateado_full", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
